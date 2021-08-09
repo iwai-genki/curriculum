b8b13ff2dd6050b548f3268bd077e294258cacee
iwai-genki <88614627+iwai-genki@users.noreply.github.com>
2021-08-09 10:42:33 +0900

    chapter1

---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("Hello Java");
+
+		System.out.println(list.get(1));
+
+	}
+
+}
